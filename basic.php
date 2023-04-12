1635f002254bef5b939c96ac6d7c890b0d75aa56
Ahmed Maruf <maruf.devops@gmail.com>
2023-04-12 15:33:15 +0600

    learning basics

---
+++ basic.php
@@ -0,0 +1,21 @@
+<?php
+
+//Examples of printf
+
+//Examples of sprintf
+
+//Examples of spaceship operator 
+
+//Examples of null colleace
+
+//Variable scoping
+
+//functions
+
+//array, shallow copy vs deep copy
+
+//json, serialize
+
+//file upload
+
+//file
