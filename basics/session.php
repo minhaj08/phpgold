4ebb0e9a7052fea465f6abc0b6105c30558b81db
Ahmed Maruf <maruf.devops@gmail.com>
2023-05-18 13:20:07 +0600

    learning

--- basics/session.php
+++ basics/session.php
@@ -12,6 +12,8 @@
  * 6. How to destroy session?
  * 7. How to set session expiry time?
  * 8. How to set session name?
- * 8. How to get same session in different pages? (when the session have specific name)
- * 
+ * 9. How to get same session in different pages? (when the session have specific name)
+ * 10. session scope, how session scope work by default and what need to do to change the session scope?
+ * 11. How session storage works internally?
+ * 12. What is cookie? and how it is different from session?
 **/
